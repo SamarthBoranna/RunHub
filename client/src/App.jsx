20912773caed0565a582cd1a290fce823e58011c
Samarth Boranna <samboranna1@gmail.com>
2025-08-23 19:32:23 -0700

    Created a Home Page and added background texture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,8 +23,16 @@ function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
-      <main className="flex-1 container mx-auto">
-        <div>
+      <main
+        className="flex-1 w-full"
+        style={{
+          backgroundImage:
+            "url(\"data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%233b82f6' fill-opacity='0.03' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 5V4zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E\")",
+          backgroundSize: "6px 6px",
+          backgroundColor: "white",
+        }}
+      >
+        <div className="container mx-auto">
           <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/runs" element={<RunsPage />} />
